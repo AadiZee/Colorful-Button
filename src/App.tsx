ffda32bc6c4aa02095e2b1f665855a22d4cd4dd6
Adnan Zaib <adnanzaib6@gmail.com>
2025-04-21 13:54:58 +0500

    First and Final Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Button from "./components/button";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Button />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/button.tsx
@@ -0,0 +1,13 @@
+import "./button.css";
+
+const Button = () => {
+  return (
+    <div className="button-border">
+      <button type="button" className="button">
+        Start Coding!
+      </button>
+    </div>
+  );
+};
+
+export default Button;
